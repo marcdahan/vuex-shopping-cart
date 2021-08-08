95809daf3736d3d04f74be27363dabf873150692
marcdahan <marcdahan@gmail.com>
2021-08-08 20:02:23 +0200

    first push

---
+++ src/components/ProductList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <ul>
+            <li v-for="product in products">{{product.title}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+import shop from '@/api/shop.js'
+export default {
+    data() {
+        return {
+            products: [],
+        }
+    },
+    created() {
+        shop.getProducts(product => {
+            this.products = product
+        })
+    }
+}
+</script>
+<style scoped>
+
+</style>
