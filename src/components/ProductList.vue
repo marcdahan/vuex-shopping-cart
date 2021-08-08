1d1e6b0fc25170797a1cdf0b2f12bffa360d8d88
marcdahan <marcdahan@gmail.com>
2021-08-09 01:47:15 +0200

    chapiter2:6

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <h1>Product List</h1>
-        <ul>
+        <img v-if="loading" src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" >
+        <ul v-else >
             <li v-for="product in products" :key="product.index" >{{product.title}}</li>
         </ul>
     </div>
@@ -10,6 +11,11 @@
 
 import store from '@/store/index'
 export default {
+    data  () {
+        return {
+            loading : false
+        }
+    },
     computed: {
         products() {
             return store.getters.availableProducts
@@ -21,7 +27,9 @@ export default {
             //store.state.products = product //not good because we can't update a state directly without calling a mutation
             store.commit('setProducts', products)
         }) */
-        store.dispatch('fetchProducts', 'toys')
+        this.loading = true
+        store.dispatch('fetchProducts'/*, 'toys'*/) // dispatch is similar to commit but dedicated to actions
+            .then(() => this.loading = false)
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -14,12 +14,22 @@ export default new Vuex.Store({
     }
   },
   actions: {//methods who never update the state
+    fetchProducts ({commit}) {
+      return new Promise ((resolve, reject) => {
+        shop.getProducts(products => {
+          //store.state.products = product //not good because we can't update a state directly without calling a mutation
+          commit('setProducts', products)
+          resolve()
+        })  
+      })
+    },
+/*  version without using the ES6 argument destructuring method which need the use of context prefix
     fetchProducts (context) {
-      shop.getProducts(products => {
+    shop.getProducts(products => {
         //store.state.products = product //not good because we can't update a state directly without calling a mutation
         context.commit('setProducts', products)
-    })
-    },
+        })
+    }, */
 /*  addToCart (context, product) {
       if (product.inventory > 0) {
         context.commit('pushProductToCart', product)
